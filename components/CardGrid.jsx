30e7aa18acd83adcf4a2e667e412458a4726629d
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-25 08:59:37 +0100

    Updated the Card grid

---
+++ components/CardGrid.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import RestaurantCard from './RestaurantCard'
+
+export default function CardGrid() {
+  const mockData = [
+    {
+      name: 'Anchor & James',
+      type: 'American',
+      location: 'Downtown Union Street',
+      price: '$30 and under',
+      reviews: '1897',
+      img: 'https://via.placeholder.com/300x200',
+    },
+    {
+      name: 'Anchor & James',
+      type: 'American',
+      location: 'Downtown Union Street',
+      price: '$51 and over',
+      reviews: '1897',
+      img: 'https://via.placeholder.com/300x200',
+    },
+  ]
+
+  return (
+    <div className="card-grid">
+      {mockData.map((item, idx) => (
+        <RestaurantCard key={idx} {...item} />
+      ))}
+    </div>
+  )
+}
