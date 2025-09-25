cb52d2e903e46f5ae90721008c02105c28dce64f
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-25 08:50:32 +0100

    updated the Hero section

---
+++ components/HeroSearch.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function HeroSearch() {
+  return (
+    <section className="hero">
+      <h1 className="hero-title">Stop looking for a restaurant - find it.</h1>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search for Restaurants by Name, Cuisine, Location"
+        />
+        <button className="btn search">Search</button>
+      </div>
+    </section>
+  )
+}
