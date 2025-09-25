fe6a82b45cddbab5924a4d06e888769b1df67697
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-25 09:00:18 +0100

    updated the footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>Copyright 2022 - 2022</p>
+      <div>
+        <a href="#">Privacy Policy</a> | <a href="#">Website Terms</a>
+      </div>
+    </footer>
+  )
+}
